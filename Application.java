1609293e7e7d883689d8e77aeacc6fc9b48c1af2
harish-netspi <harish.bhise@netspi.com>
2024-06-19 11:56:27 +0530

    added java code

---
+++ Application.java
@@ -0,0 +1,11 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Shri Swami Samarth Jay Jay Swami Samarth");
+
+        System.out.print("Arguements passed through command line are : ");
+        for(String arg : args) {
+            System.out.print(arg + " ");
+        }
+    }
+}
